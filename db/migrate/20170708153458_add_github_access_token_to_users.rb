2e49416a7a4dec0c212418352ecbdd3748918100
Pedro Piñera <pepibumur@gmail.com>
2017-07-08 17:53:15 +0200

    Fix wrong migration name

--- db/migrate/20170708153458_add_github_access_token_to_users.rb
+++ db/migrate/20170708153458_add_github_access_token_to_users.rb
@@ -1,4 +1,4 @@
-class AddGitHubAccessTokenToUsers < ActiveRecord::Migration[5.1]
+class AddGithubAccessTokenToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :github_access_token, :string
   end
