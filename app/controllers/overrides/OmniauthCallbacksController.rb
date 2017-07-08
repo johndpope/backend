9ef85b9bcf32491f99d3907ab836b9c5ee15ee9f
Pedro Piñera <pepibumur@gmail.com>
2017-07-08 13:51:55 +0200

    Override omniauth controller

--- app/controllers/overrides/OmniauthCallbacksController.rb
+++ app/controllers/overrides/OmniauthCallbacksController.rb
@@ -1,5 +1,21 @@
 module Overrides
   class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
+    def omniauth_success
+      get_resource_from_auth_hash
+      create_token_info
+      set_token_on_resource
+      create_auth_params
 
+      if resource_class.devise_modules.include?(:confirmable)
+        # don't send confirmation email!!!
+        @resource.skip_confirmation!
+      end
+
+      sign_in(:user, @resource, store: false, bypass: false)
+
+      @resource.save!
+
+      yield @resource if block_given?
+    end
   end
 end
