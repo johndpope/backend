436497329b48b35c0b4bb954bd460171710914e3
Pedro Piñera <pepibumur@gmail.com>
2017-07-09 11:37:05 +0200

    Fix user (again)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :omniauthable
 
   # Concerns
+  serialize :tokens, JSON #https://github.com/lynndylanhurley/devise_token_auth/issues/495
   include DeviseTokenAuth::Concerns::User
-
-  # https://github.com/lynndylanhurley/devise_token_auth/issues/121
-  def destroy_expired_tokens
-    if self.tokens
-      jsonTokens = JSON.parse(self.tokens)
-      jsonTokens.delete_if do |cid, v|
-        expiry = v[:expiry] || v["expiry"]
-        DateTime.strptime(expiry.to_s, '%s') < Time.now
-      end
-      self.tokens = jsonTokens.to_json
-    end
-  end
 end
