439d08d947889f0888f01ac2fcd7e9d1c744cdc5
Pedro Piñera <pepibumur@gmail.com>
2017-07-09 11:23:50 +0200

    Fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   # Concerns
   include DeviseTokenAuth::Concerns::User
 
+  # https://github.com/lynndylanhurley/devise_token_auth/issues/121
+  def destroy_expired_tokens
+    return if self.tokens == '{}'
+    super
+  end
 end
