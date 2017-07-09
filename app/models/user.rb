ae1bcad6dd8339fd74595ae056c2aa458eed2bb2
Pedro Piñera <pepibumur@gmail.com>
2017-07-09 11:32:38 +0200

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,13 @@ class User < ApplicationRecord
 
   # https://github.com/lynndylanhurley/devise_token_auth/issues/121
   def destroy_expired_tokens
-    return if self.tokens == '{}'
-    super
+    if self.tokens
+      jsonTokens = JSON.parse(self.tokens)
+      jsonTokens.delete_if do |cid, v|
+        expiry = v[:expiry] || v["expiry"]
+        DateTime.strptime(expiry.to_s, '%s') < Time.now
+      end
+      self.tokens = jsonTokens.to_json
+    end
   end
 end
